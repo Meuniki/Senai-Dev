30702cc74add80ea7d1c997d4f3ba9ee31c0709c
Meuniki <castro.rocha.rcr@gmail.com>
2021-04-27 09:47:50 -0300

    separação do banco em DQL, DDL, DML

---
+++ Spint_1-Bando_de_dados/Filmes/DDL.sql
@@ -0,0 +1,22 @@
+-- Comandos DDL
+CREATE DATABASE Filmes;
+USE Filmes;
+
+CREATE TABLE Generos(
+	idGenero	INT PRIMARY KEY IDENTITY,
+	Nome		VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE Filmes(
+	idFilme		INT PRIMARY KEY IDENTITY,
+	idGenero	INT FOREIGN KEY REFERENCES Generos (idGenero),
+	Titulo		VARCHAR(150) NOT NULL
+);
+GO
+
+CREATE TABLE Usuarios(
+	idUsuario		INT PRIMARY KEY IDENTITY,
+	email			VARCHAR(250) NOT NULL UNIQUE,
+	senha			VARCHAR(250) NOT NULL,
+	permissao		VARCHAR(250) NOT NULL
+);
